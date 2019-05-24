fa399f4b0b2cc89ed36cd867844fc8ddb38f166d
Nicolas Laval <nicolas.laval@insee.fr>
2019-05-24 10:05:45 +0200

    Update configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel';
+import builtins from 'rollup-plugin-node-builtins';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
@@ -17,6 +18,7 @@ export default {
 		sourcemap: true,
 	},
 	plugins: [
+		builtins(),
 		resolve(),
 		postcss(),
 		babel({
