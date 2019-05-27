5224ee4e5667f94c0b365bf02cdee8cdfbedb228
Nicolas Laval <nicolas.laval@insee.fr>
2019-05-27 10:13:06 +0200

    Update tests

--- src/tests/components/vtl-editor.spec.js
+++ src/tests/components/vtl-editor.spec.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { VtlEditor } from 'components';
 
+const handleValue = value => console.log('value :', value);
+const handleValid = bool => console.log('is valid :', bool);
+
 describe('vtl-editor component', () => {
 	it('renders without crashing', () => {
-		shallow(<VtlEditor />);
+		shallow(<VtlEditor handleValue={handleValue} handleValid={handleValid} />);
 	});
 });
