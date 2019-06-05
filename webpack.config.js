1e1c9c6120fe07a2bc56458e3d9a706ff807cfb3
Nicolas Laval <nicolas.laval@insee.fr>
2019-06-05 11:46:49 +0200

    Replace rollup by webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+module.exports = {
+	entry: './src/components/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'index.js',
+		libraryTarget: 'commonjs2',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src'),
+				exclude: /(node_modules|bower_components|build)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env'],
+					},
+				},
+			},
+			{
+				test: /\.(s*)css$/,
+				loaders: ['style-loader', 'css-loader', 'sass-loader'],
+				include: path.resolve(__dirname, '../'),
+			},
+		],
+	},
+	externals: {
+		react: 'commonjs react',
+	},
+	node: { fs: 'empty' },
+	plugins: [
+		new MonacoWebpackPlugin({
+			languages: ['javascript'],
+		}),
+	],
+};
